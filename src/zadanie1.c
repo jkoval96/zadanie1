e9a97f929178e93c3da474941985ccb8d7962f61
Jan Koval <jan.koval96@gmail.com>
2017-03-05 15:26:18 +0100

    add test

--- src/zadanie1.c
+++ src/zadanie1.c
@@ -1,19 +1,36 @@
 #include<stdio.h>
+#include<string.h>
 
 short int swap_endianness(short int x) {
+	int temp = (int) x;
+	
+	temp = (x << 8) | (x >> 8);
+	return (short int) temp;
+}
+
+short int swap_endianness2(short int x) {
 	short int y;
 	
 	y = (x << 8) | (x >> 8 & 0x00FF);
 	return y;
 }
 
+short int swap_endianness3(short int x) {
+	unsigned char res[2];
+	memcpy(&res, &x, 2);
+	unsigned char tmp = res[0];
+	res[0] = res[1];
+	res[1] = tmp;
+	memcpy(&x, &res, 2);
+	return x;
+}
+
 int main() {
-	short int in1 = 1;
-	short int in2 = 550;
-	short int in3 = 1775;	
-	
-	printf("%d\n", swap_endianness(in1));
-	printf("%d\n", swap_endianness(in2));
-	printf("%d\n", swap_endianness(in3));
+	int i;
+	for (i = -32767; i< 32768; i++) {
+		if (swap_endianness2(i) != swap_endianness3(i)) {
+			printf("%d\n", i);
+		}
+	}
 	return 0;
 }
